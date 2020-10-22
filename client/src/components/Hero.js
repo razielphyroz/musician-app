34c7203edd77eb5d1d24b7bac0ec40e03b3beb2f
Raziel <henrique.sonserina@hotmail.com>
2020-10-22 19:36:19 -0300

    portugueses

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <section className="hero is-danger">
+    <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
           Musician App
